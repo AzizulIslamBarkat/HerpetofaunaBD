1e51e809e355ac4eea4f498deb4d2151b630a009
AzizulIslamBarkat <azizulislambarkat@gmail.com>
2022-03-10 18:12:23 +0600

    Added Sitemap and Sitemap Generator

--- sitemap_generator.js
+++ sitemap_generator.js
@@ -1,6 +1,6 @@
 const SitemapGenerator = require('sitemap-generator');
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
-var generator = SitemapGenerator('https://www.herpetofaunabd.live/archive.html', {
+var generator = SitemapGenerator('https://www.herpetofaunabd.live/', {
   maxDepth: 0,
   filepath: './sitemap.xml',
   maxEntriesPerFile: 50000,
